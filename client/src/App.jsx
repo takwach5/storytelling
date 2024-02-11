61f8a01df0f8a5384d03d7235b1a26cb0e78e13d
takwach5 <channoufitakwa09@gmail.com>
2024-02-11 23:05:34 +0100

    finsh without css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React,{  useState,useEffect } from "react";
+import React,{ useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import Addstory from "./components/Addstory.jsx";
